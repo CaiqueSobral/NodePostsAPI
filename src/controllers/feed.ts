a97feac5a7935afa57d2d039808e6a9901215955
Caique Sobral <caiquelsobral@gmail.com>
2023-09-01 12:59:25 -0300

    Feat: mongo get posts

--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -2,41 +2,12 @@ import { Request, Response } from 'express';
 import { validationResult } from 'express-validator/src/validation-result';
 import { postModel } from '../models/post';
 
-export const getPosts = (_: Request, res: Response): void => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is a first post',
-        creator: {
-          name: 'Caique',
-        },
-        date: new Date(),
-      },
-      {
-        _id: '2',
-        title: 'Second Post',
-        content: 'This is the second post',
-        creator: {
-          name: 'Caique',
-        },
-        date: new Date(),
-      },
-      {
-        _id: '3',
-        title: 'Third Post',
-        content: 'Yet another post',
-        creator: {
-          name: 'Caique',
-        },
-        date: new Date(),
-      },
-    ],
-  });
+export const getPosts = async (_: Request, res: Response) => {
+  const posts = await postModel.find();
+  res.status(200).json(posts);
 };
 
-export const postPost = (req: Request, res: Response) => {
+export const postPost = async (req: Request, res: Response) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({
@@ -52,15 +23,13 @@ export const postPost = (req: Request, res: Response) => {
     creator: body.creator,
   });
 
-  (async () => {
-    try {
-      const result = await post.save();
-      res.status(201).json({
-        message: 'Post created successfully',
-        post: result,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  })();
+  try {
+    const result = await post.save();
+    res.status(201).json({
+      message: 'Post created successfully',
+      post: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
