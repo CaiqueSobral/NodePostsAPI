09a1001d8c8b893bd6f50c711af2ebfb45ff74af
Kiki <caiquelsobral@gmail.com>
2023-09-04 19:18:50 -0300

    Feat: Add user model

---
+++ src/models/user.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+    },
+  ],
+});
+
+export const userModel = mongoose.model('User', userSchema);
